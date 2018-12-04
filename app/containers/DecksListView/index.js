28be66212817540568b51c708a3f3bd7339984f9
Paulo de Oliveira Neto <paulo-gyn@outlook.com>
2018-12-04 15:32:01 -0200

    Layout: improve home screen
    Move "add decks" button to the bottom of the screen, in order to improve
    the UX

--- app/containers/DecksListView/index.js
+++ app/containers/DecksListView/index.js
@@ -3,68 +3,99 @@ import * as MapUtils from '../../utils/MapUtils';
 import * as Api from '../../utils/api';
 
 import {View, Text, StyleSheet} from "react-native";
-import {Container, List, ListItem, Body, Button, Icon} from 'native-base';
-import {CardsNumber, DeckTitle} from './styles';
+import {Container, Content, List, ListItem, Body, Button, Icon, Footer, FooterTab} from 'native-base';
+import {CardsNumber, DeckTitle, NoDecksMessage} from './styles';
 import {AppHeader} from '../../components/AppHeader';
+import {pink, purple, white} from "../../utils/colors";
 
 export default class DeckListView extends React.Component {
-	state = {
-		isFabActive: true,
-		decks: {}
-	};
+    state = {
+        isFabActive: true,
+        decks: {}
+    };
 
-	static navigationOptions = {
-		headerTitle: <AppHeader title='Decks'/>,
-	};
+    static navigationOptions = {
+        headerTitle: <AppHeader title='Decks'/>,
+    };
 
-	componentDidMount() {
-		Api.fetchDecks().then(decks => {
-			if(decks){
-				this.setState({decks});
-			}
-		});
-	}
+    componentDidMount() {
+        Api.fetchDecks().then(decks => {
+            if (decks) {
+                this.setState({decks});
+            }
+        });
+    }
+
+    addDeck = (deck) => {
+        const { decks } = this.state;
+        const newDecks = {...decks, [deck.key]: deck};
+
+        this.setState({decks: newDecks});
+    };
+
+    goToViewAddDeck = () => {
+        this.props.navigation.navigate(
+            'AddDeckView',
+            {addDeck: this.addDeck}
+        )
+    };
+
+    goToDeckView = deck => () => {
+        this.props.navigation.navigate(
+            'DeckView',
+            {key: deck.key, title: deck.title, cardsNumber: deck.cardsNumber}
+        )
+    };
 
 
-	render() {
-		const decksArray = this.state.decks && Object.keys(this.state.decks).length
-			? MapUtils.toArray(this.state.decks)
-			: null;
+    render() {
+        const decksArray = this.state.decks && Object.keys(this.state.decks).length
+            ? MapUtils.toArray(this.state.decks)
+            : null;
 
-		if(decksArray){
-			return (
-				<Container style={styles.decksView}>
-					<Button block primary bordered onPress={() => this.props.navigation.navigate(
-						'AddDeckView',
-						{ }
-					)}>
-						<Icon name='add' />
-						<Text>Add deck</Text>
-					</Button>
-
-					<List>
-						{decksArray.map(deck => (
-							<ListItem key={deck.key} button={true} onPress={() => this.props.navigation.navigate(
-								'DeckView',
-								{ key: deck.key, title:deck.title, cardsNumber: deck.cardsNumber }
-							)} >
-								<Body>
-								<DeckTitle>{deck.title}</DeckTitle>
-								<CardsNumber>{`${deck.cardsNumber} cards`}</CardsNumber>
-								</Body>
-							</ListItem>
-						))}
-					</List>
-				</Container>
-			);
-		}
-		return <View></View>;
-	}
+        if (decksArray) {
+            return (
+                <Container style={styles.decksView}>
+                    <Content>
+                        <List>
+                            {decksArray.map(deck => (
+                                <ListItem key={deck.key} button={true} onPress={this.goToDeckView(deck)}>
+                                    <Body>
+                                    <DeckTitle>{deck.title}</DeckTitle>
+                                    <CardsNumber>{`${deck.cardsNumber} cards`}</CardsNumber>
+                                    </Body>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Content>
+                    <Footer>
+                        <FooterTab style={{backgroundColor: pink, elevation: 6}}>
+                            <Button full onPress={this.goToViewAddDeck}>
+                                <Text style={{color: white, fontSize: 18}}>ADD DECK</Text>
+                            </Button>
+                        </FooterTab>
+                    </Footer>
+                </Container>
+            );
+        }
+        return <Container style={styles.decksView}>
+            <Content>
+                <NoDecksMessage>There are no decks to show</NoDecksMessage>
+            </Content>
+            <Footer>
+                <FooterTab>
+                    <Button full primary onPress={this.goToViewAddDeck}>
+                        <Text style={{color: white, fontSize: 16}}>ADD DECK</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Container>;
+    }
 }
 
 export const styles = StyleSheet.create({
-	decksView: {
-		flex: 1,
-		margin: 16,
-	},
+    decksView: {
+        flex: 1,
+        // margin: 16,
+    },
 });
--- app/containers/DecksListView/styles.js
+++ app/containers/DecksListView/styles.js
@@ -4,6 +4,10 @@ export const DeckTitle = styled.Text`
 	font-size: 18;
 `;
 
+export const NoDecksMessage = styled.Text`
+	font-size: 18;
+`;
+
 export const CardsNumber = styled.Text`
 	font-size: 12;
 	color: grey;
